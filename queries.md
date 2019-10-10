3581beab820fb285d2e4cfac009c981956ddcf93
noahfranco <noahfra@gmail.com>
2019-10-10 11:40:40 -0400

    finished findSteps. All endpoints are working

--- queries.md
+++ queries.md
@@ -6,7 +6,7 @@ Answer: SELECT ProductName, CategoryName FROM Products AS P JOIN                
 
 ### Display the OrderID and ShipperName for all orders placed before January 9, 1997. Shows 161 records.
 
-Answer: 
+Answer: SELECT OrderID, ShipperName, OrderDate FROM Orders, Shippers  
 
 ### Display all ProductNames and Quantities placed on order 10251. Sort by ProductName. Shows 3 records.
 
--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -21,12 +21,18 @@ function findById(id) {
     .where({ id }).first()
 }
 
+// Don't understand how to do this at all 
 // .get() post from /:id/steps ~ With SQL 
 function findSteps(id) {
-    // return db("schemes")
-    return db.select("*")
-    .where("schemes as P")
-    .join("")
+    return db("steps")
+    .innerJoin(
+    "schemes", 
+    "steps.scheme_id", 
+    "schemes.id"
+    ) 
+    .select("steps.id", "schemes.scheme_name", "steps.step_number", "steps.instructions")
+    .where("steps.scheme_id", id)
+    .orderBy("steps.step_number")
 }
 
 function add(scheme) {
